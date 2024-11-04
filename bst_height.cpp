9cc431c0f432c526888e49c814b67cc2e88f68d1
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2024-11-04 08:17:47 +0500

    Update bst_height.cpp

--- bst_height.cpp
+++ bst_height.cpp
@@ -48,6 +48,41 @@ bool search(Node* root, int key){
     else return search(root->right, key);   
 }
 
+Node* findMin(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL) return root;
+
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } 
+    else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } 
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } 
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
 int calcheight(Node* root){
     if(root == NULL){
         return -1;
